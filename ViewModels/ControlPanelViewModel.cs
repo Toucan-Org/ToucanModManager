22761f98ba7c2a2db74c8008c13dda6c9d1c6e62
Loki_Lokster <1657477+Loki-Lokster@users.noreply.github.com>
2023-04-10 10:15:57 +1000

    Added ViewModels + Views
    Added new ViewModels and Views for Header and Control Panel. Added these both to use the MainWindow DataContext so now all panels can communicate with each other.
    
    Also added some placeholders for the search bar and other buttons as well as a new `View` menu in the File Menu at the top.

---
+++ ViewModels/ControlPanelViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToucanUI.ViewModels
+{
+    public class ControlPanelViewModel : ViewModelBase
+    {
+        public MainWindowViewModel MainViewModel { get; }
+
+        public ControlPanelViewModel(MainWindowViewModel mainViewModel)
+        {
+            MainViewModel = mainViewModel;
+        }
+    }
+}
