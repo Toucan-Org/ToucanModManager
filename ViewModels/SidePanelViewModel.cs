9c5c909b44bc5cf0084989976ce804f474a6fcf1
Loki_Lokster <1657477+Loki-Lokster@users.noreply.github.com>
2023-04-11 15:34:25 +1000

    UI Polish
    UI Polish pass and added X to side panel in order to close it.

--- ViewModels/SidePanelViewModel.cs
+++ ViewModels/SidePanelViewModel.cs
@@ -10,6 +10,7 @@ using System.IO;
 using System.Reactive;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using ToucanUI.Models;
 using ToucanUI.Services;
 
@@ -19,12 +20,28 @@ namespace ToucanUI.ViewModels
     {
         //Create a MainViewModel as a parent
         public MainWindowViewModel MainViewModel { get; }
+
+        public ICommand OpenGitHubCommand { get; }
+        public ICommand CloseSidePanelCommand { get; }
+
+
+
         public SidePanelViewModel(MainWindowViewModel mainViewModel) 
         {
             //Has to be passed into the child constructors
             MainViewModel = mainViewModel;
 
-         
+            OpenGitHubCommand = ReactiveCommand.Create(() =>
+            {
+                // Navigate to your GitHub page
+                Process.Start(new ProcessStartInfo("https://github.com/KSP2-Toucan/ToucanModManager") { UseShellExecute = true });
+            });
+
+            CloseSidePanelCommand = ReactiveCommand.Create(() =>
+            {
+                SidePanelVisible = false;
+            });
+
         }
 
         private bool _sidePanelVisible = false;
@@ -34,6 +51,9 @@ namespace ToucanUI.ViewModels
             set => this.RaiseAndSetIfChanged(ref _sidePanelVisible, value);
             
         }
+
+
+
 
         
        
