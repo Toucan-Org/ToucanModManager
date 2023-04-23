d47ea9b0225babde29c1f58b424239f464f388ab
Loki_Lokster <1657477+Loki-Lokster@users.noreply.github.com>
2023-04-23 22:54:00 +1000

    Squashed commit of the following:
    commit dd04083f23793e6efb4560e712d03de04db71ef9
    Author: Caleb <72399776+calebh210@users.noreply.github.com>
    Date:   Sun Apr 23 12:51:30 2023 +0100
    
        Added filters
    
    commit a273bf1c20b63b82c41a40ce1097087f94bb6357
    Author: Caleb <72399776+calebh210@users.noreply.github.com>
    Date:   Tue Apr 18 01:35:14 2023 +0200
    
        Searchbar functionality addded

--- ViewModels/ModlistViewModel.cs
+++ ViewModels/ModlistViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Platform;
+using DynamicData;
+using DynamicData.Binding;
 using ReactiveUI;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reactive;
+using System.Reactive.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using ToucanUI.Models;
@@ -19,11 +24,16 @@ namespace ToucanUI.ViewModels
     {
         SpacedockAPI api = new SpacedockAPI();
 
-        public ObservableCollection<Mod> Mods { get; set; }
+        private ReadOnlyObservableCollection<Mod> _mods;
+        public ReadOnlyObservableCollection<Mod> Mods { get => _mods; set => this.RaiseAndSetIfChanged(ref _mods, value); }
+        
+
+        public ObservableCollection<Mod> ModList { get; set; }
 
         public ReactiveCommand<Mod, Unit> DownloadMod { get; }
         public ReactiveCommand<Unit, Unit> ToggleViewCommand { get; }
 
+        //private SourceList<Mod> _sourceList { get; set; }
 
         private Mod _selectedMod;
         public Mod SelectedMod
@@ -58,22 +68,94 @@ namespace ToucanUI.ViewModels
 
             DownloadMod = ReactiveCommand.Create<Mod>(mod => DownloadModAsync(mod));
             ToggleViewCommand = ReactiveCommand.Create(SwitchView);
+
             LoadMods(true);
-            
+
+            //This is the filter that is applied to the sourcelist
+            var observableSearchFilter = this.WhenAnyValue(viewModel => viewModel.SearchText).Select(SearchName);
+            var InstalledFilter = this.WhenAnyValue(x => x.MainViewModel.ControlPanelVM.FilterInstalled).Select(SetInstalledFilter);
+            var VersionFilter = this.WhenAnyValue(x => x.MainViewModel.ControlPanelVM.FilterVersion).Select(SetVersionFilter);
+            //this converts the modlist (which contains the mods recieved from the API) into an Observable List
+            var _sourceList = ModList
+               .ToObservableChangeSet()
+               .Sort(SortExpressionComparer<Mod>.Ascending(x => x.Name)) //Sorts list by name
+               .Filter(observableSearchFilter) //Applies the filter
+               .Filter(InstalledFilter)
+               .Filter(VersionFilter)
+               .AsObservableList();
+
+            _sourceList.Connect()
+               .Bind(out _mods) //Binds the newly created sourcelist to the mod ObservableCollection (which is displayed in the axaml)
+               .DisposeMany()
+               .Subscribe();
+
+
         }
+
+        private Func<Mod, bool> SearchName(string name)
+        {
+            if (string.IsNullOrEmpty(name)) //If searchbar is empty, show all the mods
+            {
+                return Mod => true;
+            }
+            else
+            {
+                return Mod => Mod.Name.Contains(name, StringComparison.OrdinalIgnoreCase);
+            }
+        }
+
+        private Func<Mod, bool> SetInstalledFilter(bool isOn)
+        {
+            if (isOn)
+            {
+                return Mod => Mod.IsInstalled;
+            }
+            else
+            {
+                return Mod => true;
+            }
+        }
+
+        private Func<Mod, bool> SetVersionFilter(bool isOn)
+        {
+            if (isOn)
+            {
+                return Mod => Mod.GameVersion.Equals("0.1.0.0");
+            }
+            else
+            {
+                return Mod => true;
+            }
+        }
+
+        private string _searchText;
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _searchText, value);
+                //Clear the side panel if the user types - fixes visual bug
+                MainViewModel.SelectedMod = null;
+                MainViewModel.SidePanelVM.SidePanelVisible = false;
+               
+            }
+        }
+
 
         public void SwitchView()
         {
             IsClassicViewVisible = !IsClassicViewVisible;
             IsGridViewVisible = !IsGridViewVisible;
+
         }
 
         // Load the mod list from the API
         private async Task LoadMods(bool useDummyData = false)
         {
             var mods = await api.GetMods(useDummyData);
-            Mods = new ObservableCollection<Mod>(mods);
-            foreach (var mod in Mods)
+            ModList = new ObservableCollection<Mod>(mods);
+            foreach (var mod in mods)
             {
                 Debug.WriteLine("Name: " + mod.Name);
                 Debug.WriteLine("Id: " + mod.Id);
@@ -103,6 +185,7 @@ namespace ToucanUI.ViewModels
         {
             return mod == SelectedMod;
         }
+
 
 
     }
