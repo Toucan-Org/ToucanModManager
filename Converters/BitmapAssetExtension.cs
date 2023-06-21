dfe3650e38c201a438dcb43031e25aebc1b15576
Loki_Lokster <1657477+Loki-Lokster@users.noreply.github.com>
2023-06-21 13:14:00 +1000

    More OSX/Linux fixes
    Fixed some more issues with pathing on MacOS and linux. Still more issues to fix.

---
+++ Converters/BitmapAssetExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+
+namespace ToucanUI.Converters
+{
+    public class BitmapAssetExtension : MarkupExtension
+    {
+        public string AssetPath { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (!string.IsNullOrEmpty(AssetPath))
+            {
+                string currentAssemblyPath = AppContext.BaseDirectory;
+                string fullPath = Path.Combine(currentAssemblyPath, AssetPath.Replace('/', Path.DirectorySeparatorChar));
+                return new Bitmap(fullPath);
+            }
+
+            return null;
+        }
+    }
+
+}
