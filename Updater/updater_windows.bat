ff5c94b71038b14b45d2ae757e89a7a50e1853f5
Loki_Lokster <1657477+Loki-Lokster@users.noreply.github.com>
2023-06-27 21:27:33 +1000

    Updater fixes
    Fixed some issues with the windows self updater, still not working properly on MacOS or linux

--- Updater/updater_windows.bat
+++ Updater/updater_windows.bat
@@ -3,25 +3,20 @@
 :: Step 1: Read the download URL from "updater_config.txt" and download the zip file
 set "updaterConfig=updater_config.txt"
 set /p downloadUrl=<"%updaterConfig%"
+echo Download URL: %downloadUrl%
 set "zipFile=new_version.zip"
 echo Downloading newest version of Toucan Mod Manager...
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%downloadUrl%', '%zipFile%')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile(\"%downloadUrl%\", \"%zipFile%\")"
 
 :: Step 2: Extract the zip file into the parent directory
 cd ..
 echo Extracting zip file...
-powershell -Command "Expand-Archive -Path 'Updater\%zipFile%' -DestinationPath '.'"
+powershell -Command "Expand-Archive -Path 'Updater\%zipFile%' -DestinationPath '.' -Force"
 
 
-:: Step 3: Ensure InstalledMods remains intact
-if not exist "InstalledMods" (
-    echo Warning: InstalledMods folder not found.
-)
-
-:: Step 4: Clean up
+:: Step 3: Clean up
 del "Updater\%zipFile%"
 del "Updater\%updaterConfig%"
-
 
 :: Step 4: Start the new ToucanUI.exe and shut down the updater script
 echo Update complete. Starting Toucan Mod Manager...
